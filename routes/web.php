8da109c4579ecf42ce70e4fc2dbe6aca36175970
acmama2526 <mamahaulu8@yahoo.co.jp>
2026-09-25 16:11:49 +0900

    画面Bのroute

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,23 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ReservationController;
+use App\Livewire\ReservationList;
+use App\Livewire\ReservationCreate;
+use App\Livewire\ReservationShow;
+use App\Livewire\ReservationEdit;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/reservations', ReservationList::class)
+    ->name('reservations.index');
 
-Route::get('/reservations', [ReservationController::class, 'index'])->name('reservations.index');
+Route::get('/reservations/create', ReservationCreate::class)
+    ->name('reservations.create');
+
+Route::get('/reservations/{reservation}', ReservationShow::class)
+    ->name('reservations.show');
+
+Route::get('/reservations/{reservation}/edit', ReservationEdit::class)
+    ->name('reservations.edit');
