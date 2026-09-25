1c2ed1785c1231a93373d53dfed3d8686288acea
pandasutepanda-create <panda.sutepanda@gmail.com>
2026-09-25 14:49:53 +0900

    初期トップページ機能9/25

---
+++ resources/views/top.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>予約管理システム</title>
+</head>
+
+<body>
+
+    <h1>担当A：予約状況画面</h1>
+
+    <p>予約管理システムTOPページ</p>
+
+</body>
+</html>
