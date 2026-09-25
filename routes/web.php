cf0c6516c3f727fe3e40487fed85955d178ce8b6
tamagoro <tamagoro202104@gmail.com>
2026-09-25 15:19:23 +0900

    Implement seat management

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,19 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ReservationController;
+use App\Livewire\SeatManager;
 
 Route::get('/', function () {
-    return view('welcome');
+  return view('welcome');
 });
 
 
-Route::get('/reservations', [ReservationController::class, 'index'])->name('reservations.index');
+Route::get(
+  '/reservations',
+  [ReservationController::class, 'index']
+)->name('reservations.index');
+
+Route::get(
+  '/seats',
+  SeatManager::class
+)->name('seats.index');
