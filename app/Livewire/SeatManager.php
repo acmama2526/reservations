0535cf0021a09854ad8d7aa1d614852c25bfc06a
tamagoro <tamagoro202104@gmail.com>
2026-09-25 13:31:48 +0900

    Add SeatManager

--- app/Livewire/SeatManager.php
+++ app/Livewire/SeatManager.php
@@ -9,6 +9,7 @@ class SeatManager extends Component
 {
   public function render()
   {
+    //seats DBを読みdisplay_order順に並べ,$seatsに入れる
     $seats = Seat::orderBy('display_order')->get();
 
     return view('livewire.seat-manager', [
